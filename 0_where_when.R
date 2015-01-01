41d5684f1be60e933ae780038792241e30736656
Salah Chafik <salahchafik@gmail.com>
2015-01-01 09:51:49 -0500

    changing url to just quotes

--- 0_where_when.R
+++ 0_where_when.R
@@ -9,7 +9,7 @@
 #1) copy and paste the url into the quotes below
 ## e.g. el_url <- "http://islamicfinder.org/prayerPrintableYearly....."
 
-el_url <- "http://www.islamicfinder.org/prayerPrintableYearly.php?city2=Bronx&state=NY&id=3447&country2=Usa&month=01&zipcode=&today_date_flag=&timez=-5.0&pmethod=5&lati=40.8208&longi=-73.9235&HanfiShafi=1&dhuhrInterval=1&maghribInterval=1&dayLight=1&dayl=1&dayLight_self_change=&prayerCustomize=&lang=english&fajrTwilight=0&ishaTwilight=0&ishaInterval=0&year=2015"
+el_url <- ""
 
 #2) change el_año to the appropriate year (default is 2015)
 
